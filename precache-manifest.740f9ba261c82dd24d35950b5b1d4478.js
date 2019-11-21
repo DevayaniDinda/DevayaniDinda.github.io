795f78231da400e457307dd17ef2f351eee8d3df
= <=>
2019-11-21 12:39:18 +0700

    my first commit

---
+++ precache-manifest.740f9ba261c82dd24d35950b5b1d4478.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a2b9ad9688757fbf56",
+    "url": "/css/chunk-vendors.b2722a82.css"
+  },
+  {
+    "revision": "27e8b6231c5efb3cd284488c42fe36f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd720da935dbd3e46a2c",
+    "url": "/js/about.690a2745.js"
+  },
+  {
+    "revision": "705a593a3894e1bacbe3",
+    "url": "/js/app.4723ce5a.js"
+  },
+  {
+    "revision": "66a2b9ad9688757fbf56",
+    "url": "/js/chunk-vendors.0db190bd.js"
+  },
+  {
+    "revision": "9288de7a9b862ae798095874a060b74e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
